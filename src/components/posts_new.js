c77fc69a9ef67da07a2d704d66bd81e9714550f2
nbkhope <ryumemaru@yahoo.com>
2016-09-08 12:38:47 -0700

    Change PostsNewForm feedback messages to have the correct Bootstrap class (form-control-feedback) so text is red when input is invalid

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { reduxForm } from 'redux-form';
+import { Link } from 'react-router';
 
 import { createPost } from '../actions/index';
 
@@ -18,7 +19,7 @@ class PostsNew extends Component {
           <div className={`form-group ${title.touched && title.invalid ? 'has-danger' : ''}`}>
             <label>Title</label>
             <input type="text" className="form-control" {...title} />
-            <div className="text-help">
+            <div className="form-control-feedback">
               {title.touched ? title.error : ''}
             </div>
           </div>
@@ -26,7 +27,7 @@ class PostsNew extends Component {
           <div className={`form-group ${categories.touched && categories.invalid ? 'has-danger' : ''}`}>
             <label>Categories</label>
             <input type="text" className="form-control" {...categories} />
-            <div className="text-help">
+            <div className="form-control-feedback">
               {categories.touched ? categories.error : ''}
             </div>
           </div>
@@ -34,12 +35,13 @@ class PostsNew extends Component {
           <div className={`form-group ${content.touched && content.invalid ? 'has-danger' : ''}`}>
             <label>Content</label>
             <textarea className="form-control" {...content} />
-            <div className="text-help">
+            <div className="form-control-feedback">
               {content.touched ? content.error : ''}
             </div>
           </div>
 
           <button type="submit" className="btn btn-primary">Create</button>
+          <Link to="/" className="btn btn-default">Create</Link>
         </form>
       </div>
     );
